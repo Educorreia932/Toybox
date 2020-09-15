c4cc55aa5a8250edd80cefdcee6cca0a44db5248
Educorreia932 <skelozard1@gmail.com>
2020-09-15 17:03:36 +0100

    Updating the handles

--- main.py
+++ main.py
@@ -21,11 +21,7 @@ canvas.pack(fill="both", expand=True)
 
 radius = SCREEN_HEIGHT / 2 - 50
 
-canvas.create_circle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, radius, width = 5)
-
-now = datetime.datetime.now()
-
-def draw_hour_handle():
+def draw_hour_handle(now):
     hour = now.hour % 12 + now.minute / 60 
     percentage = hour / 12 
     angle = percentage * 2 * pi - pi / 2 
@@ -35,7 +31,7 @@ def draw_hour_handle():
     
     canvas.create_line(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, x, y, width = 10)
     
-def draw_minute_handle():
+def draw_minute_handle(now):
     minutes = now.minute + now.second / 60 
     percentage = minutes / 60
     angle = percentage * 2 * pi - pi / 2 
@@ -45,7 +41,7 @@ def draw_minute_handle():
     
     canvas.create_line(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, x, y, width = 5)
     
-def draw_second_handle():
+def draw_second_handle(now):
     seconds = now.second
     percentage = seconds / 60
     angle = percentage * 2 * pi - pi / 2 
@@ -69,14 +65,22 @@ def draw_ticks():
         
         canvas.create_line(x0, y0, x1, y1, width = 2)
     
-draw_hour_handle()
-draw_minute_handle()
-draw_second_handle()
-draw_ticks()
+def draw():
+    now = datetime.datetime.now()
+    
+    canvas.create_circle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, radius, width = 5)
+    draw_hour_handle(now)
+    draw_minute_handle(now)
+    draw_second_handle(now)
+    draw_ticks()
 
-# while True:
-#     time.sleep(1)
-#     l[ "text" ]=time.strftime( "%d/%m/%Y %A %H:%M:%S" )
-#     root.update()
+def tick():
+    canvas.delete(tk.ALL)
 
-root.mainloop() 
+    draw()
+        
+    canvas.after(20, tick)
+
+tick()
+
+tk.mainloop()
