f841a4737f475f78aabe7392781690737e596d9e
Educorreia932 <skelozard1@gmail.com>
2020-09-15 17:23:04 +0100

    Setted the handles' size proportional to the radius

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from math import cos, sin, pi
 
 import datetime
 import tkinter as tk 
-import time
 
 def _create_circle(self, x, y, radius, **kwargs):
     return self.create_oval(x - radius, y - radius, x + radius, y + radius, **kwargs)
@@ -10,7 +9,7 @@ def _create_circle(self, x, y, radius, **kwargs):
 tk.Canvas.create_circle = _create_circle
 
 root = tk.Tk()  
-# root.attributes('-fullscreen', True)
+root.attributes('-fullscreen', True)
 root.title("Clock")
 
 SCREEN_WIDTH = root.winfo_screenwidth()
@@ -26,8 +25,8 @@ def draw_hour_handle(now):
     percentage = hour / 12 
     angle = percentage * 2 * pi - pi / 2 
 
-    x = cos(angle) * 350 + SCREEN_WIDTH / 2
-    y = sin(angle) * 350 + SCREEN_HEIGHT / 2
+    x = cos(angle) * radius * 0.60 + SCREEN_WIDTH / 2
+    y = sin(angle) * radius * 0.60 + SCREEN_HEIGHT / 2
     
     canvas.create_line(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, x, y, width = 10)
     
@@ -36,8 +35,8 @@ def draw_minute_handle(now):
     percentage = minutes / 60
     angle = percentage * 2 * pi - pi / 2 
 
-    x = cos(angle) * (radius - 70) + SCREEN_WIDTH / 2
-    y = sin(angle) * (radius - 70) + SCREEN_HEIGHT / 2
+    x = cos(angle) * radius * 0.87 + SCREEN_WIDTH / 2
+    y = sin(angle) * radius * 0.87 + SCREEN_HEIGHT / 2
     
     canvas.create_line(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, x, y, width = 5)
     
@@ -46,8 +45,8 @@ def draw_second_handle(now):
     percentage = seconds / 60
     angle = percentage * 2 * pi - pi / 2 
 
-    x = cos(angle) * (radius - 70) + SCREEN_WIDTH / 2
-    y = sin(angle) * (radius - 70) + SCREEN_HEIGHT / 2
+    x = cos(angle) * radius * 0.90 + SCREEN_WIDTH / 2
+    y = sin(angle) * radius * 0.90 + SCREEN_HEIGHT / 2
     
     canvas.create_line(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, x, y, width = 3, fill='red')
     
@@ -57,8 +56,8 @@ def draw_ticks():
         percentage = hour / 12 
         angle = percentage * 2 * pi - pi / 2 
         
-        x0 = cos(angle) * (radius - 75) + SCREEN_WIDTH / 2
-        y0 = sin(angle) * (radius - 75) + SCREEN_HEIGHT / 2
+        x0 = cos(angle) * radius * 0.85 + SCREEN_WIDTH / 2
+        y0 = sin(angle) * radius * 0.85 + SCREEN_HEIGHT / 2
     
         x1 = cos(angle) * radius + SCREEN_WIDTH / 2
         y1 = sin(angle) * radius + SCREEN_HEIGHT / 2
@@ -76,10 +75,8 @@ def draw():
 
 def tick():
     canvas.delete(tk.ALL)
-
-    draw()
-        
-    canvas.after(20, tick)
+    draw()        
+    canvas.after(66, tick) # 30 FPS Update rate
 
 tick()
 
