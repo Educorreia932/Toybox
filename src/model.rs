b270e3981fcb56c04189759918311bcc1c77e1da
Educorreia932 <skelozard1@gmail.com>
2021-01-21 15:25:55 +0000

    Reading vertices and faces information to object model

--- src/model.rs
+++ src/model.rs
@@ -2,23 +2,44 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub struct Model {
-    verts: Vec<i32>,
+    verts: Vec<Vec<f32>>,
     faces: Vec<i32>,
 }
 
 impl Model {
     pub fn from_file(filename: &str) -> Self {
-        let faces = Vec::<i32>::new();
+        let mut verts: Vec<Vec<f32>> = Vec::new();
+        let mut faces: Vec::<i32> = Vec::new();
 
         let file = File::open(filename).unwrap();
         let reader = BufReader::new(file);
 
         for line in reader.lines() {
             let line = line.unwrap();
+
+            if line.chars().count() >= 2 {
+                let s = line[0..2].trim();
+
+                if s == "v" {
+                    let line_verts = line[2..].trim().split(' ').flat_map(str::parse::<f32>).collect();
+                    verts.push(line_verts);
+                }   
+    
+                else if line[0..2].trim() == "f" {
+                    for face in line[2..].trim().split(' ') {
+                        let first: &str = face.split("/").collect::<Vec<&str>>()[0];
+                        faces.push(first.parse::<i32>().unwrap());
+                    }
+                }
+            }
         }
 
+        println!("Read model with success");
+        println!("{} vertices", verts.len());
+        println!("{} faces", faces.len());
+
         return Model {
-            verts: Vec::new(),
+            verts,
             faces
         }
     }
