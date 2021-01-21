b2cc4ac4fd6efeaaedd7178a3b0871e774e82070
Educorreia932 <skelozard1@gmail.com>
2021-01-21 00:53:39 +0000

    Improved line drawing algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
 use image::{RgbImage, Rgb, ImageBuffer};
+use std::mem;
 
-fn line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut RgbImage) {
-    for t in 0..100 { 
-        let t = t as f32 * 0.01;
+fn line(mut x0: i32, mut y0: i32, mut x1: i32, mut y1: i32, image: &mut RgbImage, color: Rgb<u8>) {
+    let mut steep = false;
 
-        let x = (x0 as f32 + (x1 as f32 - x0 as f32) * t) as u32; 
-        let y = (y0 as f32 + (y1 as f32 - y0 as f32) * t) as u32; 
+    if (x0 - x1).abs() < (y0 - y1).abs() {
+        mem::swap(&mut x0, &mut y0);
+        mem::swap(&mut x1, &mut y1);
+        steep = true;
+    }
 
-        (*image).put_pixel(x, y, Rgb([255, 0, 0]));
-    } 
+    if x0 > x1 {
+        mem::swap(&mut x0, &mut x1);
+        mem::swap(&mut y0, &mut y1);
+    }
+
+    let dx = x1 - x0;
+    let dy = y1 - y0;
+
+    let derror2 = (dy * 2).abs();
+    let mut error2 =  0;
+    let mut y = 0;
+
+    for x in x0..=x1 {
+        if steep {
+            (*image).put_pixel(y as u32, x as u32, color);
+        }
+
+        else {
+            (*image).put_pixel(x as u32, y as u32, color);
+        }
+
+        error2 += derror2;
+
+        if error2 > dx {
+            y += if y1 > y0 { 1 } else { -1 }; 
+            error2 -= dx * 2;
+        }
+    }
 }
 
 fn main() {
@@ -17,7 +46,8 @@ fn main() {
 
     let mut image : RgbImage = ImageBuffer::new(width, height);
 
-    line(13, 20, 80, 40, &mut image); 
+    line(0, 0, 50, 50, &mut image, Rgb([255, 255, 255])); 
+    line(0, 0, 10, 50, &mut image, Rgb([255, 0, 0])); 
 
     image.save("image.png").unwrap();
 }
