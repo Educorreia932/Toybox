9e9c5df2e461da5455d7559b0eb6460f420576f0
Educorreia932 <skelozard1@gmail.com>
2021-01-21 00:04:20 +0000

    Drawing a line using Bresenham’s Line Drawing Algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
+use image::{RgbImage, Rgb, ImageBuffer};
+
+fn line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut RgbImage) {
+    for t in 0..100 { 
+        let t = t as f32 * 0.01;
+
+        let x = (x0 as f32 + (x1 as f32 - x0 as f32) * t) as u32; 
+        let y = (y0 as f32 + (y1 as f32 - y0 as f32) * t) as u32; 
+
+        (*image).put_pixel(x, y, Rgb([255, 0, 0]));
+    } 
+}
+
 fn main() {
-    let imgx = 800;
-    let imgy = 800;
+    let width = 100;
+    let height = 100;
 
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 3.0 / imgy as f32;
+    let mut image : RgbImage = ImageBuffer::new(width, height);
 
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    line(13, 20, 80, 40, &mut image); 
 
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.3 * x as f32) as u8;
-        let b = (0.3 * y as f32) as u8;
-        *pixel = image::Rgb([r, 0, b]);
-    }
-
-    imgbuf.save("image.png").unwrap();
+    image.save("image.png").unwrap();
 }
